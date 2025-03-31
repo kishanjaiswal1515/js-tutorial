b3afe1d53e2b2580434a5051c54d78f959b21d5c
kishanjaiswal1515 <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-03-31 12:28:27 +0000

    changess

--- basics_js/conversionOperations.js
+++ basics_js/conversionOperations.js
@@ -1,9 +1,33 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// operations 
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(5%4);
+
+// let str1 = "hello"
+// let str2 = " karan"
+// let str3 = str1+str2
+// console.log(str3);
+
+console.log("1"+2);
+console.log(1 +"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
 
 
---
+++ basics_js/variables.js
@@ -0,0 +1,14 @@
+const accountId = 21644
+let accountEmail = "kishan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhopal"
+
+// accountId = 2
+
+accountEmail = "kish@n.com"
+accountPassword = "222222"
+accountCity = "Delhi"
+
+// prefer not to use var , insted use let and const.
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
