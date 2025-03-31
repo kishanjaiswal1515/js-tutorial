ef42acf637279c0c43b6a5f3bed4ddb051745046
kishanjaiswal1515 <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-03-31 04:40:41 +0000

    dataType

---
+++ basics_js/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3+3)  we are using nodejs , not browser
+ 
+console.log(3+3)
+
+console.log("kishan")
+
+// number -- 2 to power 53
+// bigInt
+// String
+// null
+// boolean
+// symbol -- unique
+
+console.log(typeof null);
+console.log(typeof undefined);
+
