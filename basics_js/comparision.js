00170e5faae62c83df1589f08adc39e2c9bf68a6
kishanjaiswal1515 <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-05-21 00:05:32 +0530

    Delete basics_js/comparision.js

--- basics_js/comparision.js
+++
@@ -1,9 +0,0 @@
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
-
-console.log("2" === 2);  //strict check
-console.log(2 === 2);
