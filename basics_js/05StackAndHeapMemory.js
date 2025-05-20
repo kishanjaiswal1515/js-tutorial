e831104cfd617c00c95ac7ba7723f473ee5fdc7d
kishanjaiswal1515 <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-05-20 18:28:43 +0000

    changescommited

---
+++ basics_js/05StackAndHeapMemory.js
@@ -0,0 +1,16 @@
+let myName = "kishanjaiswal";
+let anotherName = myName; 
+anotherName = "kishan";
+
+// console.log(anotherName);
+// console.log(myName);
+
+let userOne = {
+    Name : "Kishan",
+    Email : "kishan@gmail.com"
+}
+let userTwo = userOne;
+
+userTwo.Email = "kish@gmail.com"
+console.log(userOne.Email);
+console.log(userTwo.Email);
