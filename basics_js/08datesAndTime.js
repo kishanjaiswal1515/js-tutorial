fdd363ec8e2865b898bd1ed015490385ffbb53cc
kishanjaiswal1515 <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-05-20 18:26:45 +0000

    changescommited

---
+++ basics_js/08datesAndTime.js
@@ -0,0 +1,14 @@
+// let myDate = new Date();
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2025,0,22);
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleDateString());
+
+let myCreatedDate = new Date(2025,0,22,5,33);
+console.log(myCreatedDate.toLocaleString());
