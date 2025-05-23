6df4c62e94e8d9767091abab03ab858307147b69
Kishan Jaiswal <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-05-23 16:10:57 +0000

    arrays changes done

---
+++ basics_js/09arrays.js
@@ -0,0 +1,11 @@
+const myArr = [10,15,124,25,23,14]
+const myHeors = ["shaktiman", "naagraj"]
+
+console.log(myArr.length);
+console.log(myHeors.length);
+console.log(myArr[2]);
+console.log(myHeors[1]);
+
+
+const myArr2 = new Array(11,22,25,35,45)
+console.log(myArr2[1]);
