83a8620288da5b4e278eb98ba4ed920731384b5b
Kishan Jaiswal <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-08-24 20:34:19 +0000

    changes saved

---
+++ basics_js/10arrays.js
@@ -0,0 +1,7 @@
+const myArr = [10,15,124,25,23,14]
+const myHeors = ["shaktiman", "naagraj"]
+
+myArr.length = 7;
+myHeors.length = 1;
+console.log("increasing " ,myArr);
+console.log("decreasiing ", myHeors);
