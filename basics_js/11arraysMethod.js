a8a2023426c0100aeb84a0e1c92d4db4bace69d1
Kishan Jaiswal <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-08-28 09:24:18 +0000

    done

---
+++ basics_js/11arraysMethod.js
@@ -0,0 +1,19 @@
+// JavaScript Arrays Method
+
+
+//delete 
+
+let emp = {
+    FirstName : "Kishan",
+    LastName : "Jaiswal",
+    Salary: 100000
+}
+console.log(delete emp.Salary);
+console.log(emp);
+
+
+//join 
+
+let a = ["HTML","CSS","JS","React"];
+console.log(a.join('|'));
+
