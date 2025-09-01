8ae7951993715ca93eddfd7143e8026f94b14012
Kishan Jaiswal <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-09-01 07:03:10 +0000

    new methods added

--- basics_js/11arraysMethod.js
+++ basics_js/11arraysMethod.js
@@ -14,6 +14,47 @@ console.log(emp);
 
 //join 
 
-let a = ["HTML","CSS","JS","React"];
+let a = ["HTML ","CSS ","JS ","React"];
 console.log(a.join('|'));
 
+//The concat() method is used to concatenate two or more arrays and it gives the merged array
+
+let a1 = [12,13,14];
+let a2 = [15,16,17];
+console.log(a1.concat(a2));
+
+//The push() method is used to add an element at the end of an Array
+
+let a3 = [10, 20, 30, 40, 50];
+a3.push(60);
+console.log(a3);
+
+
+//The unshift() method is used to add elements to the front of an Array.
+let a4 = [20, 30, 40];
+a4.unshift(10,15);
+console.log(a4);
+
+//The pop() method is used to remove elements from the end of an array. 
+let a5 = [20, 30, 40, 50];
+a5.pop();
+console.log(a5);
+
+//The shift() method is used to remove elements from the beginning of an array 
+let a6 = [20, 30, 40, 50];
+a6.shift();
+console.log(a6);
+
+
+//The splice() method is used to Insert and Remove elements in between the Array.
+//array.splice(start, deleteCount, item1, item2, ...);
+//start → index jaha se operation shuru karna hai.
+
+// start → index jaha se operation shuru karna hai.
+// deleteCount → kitne elements delete karne hain. (agar 0 doge to delete nhi hoga).
+// item1, item2, ... → (optional) naye elements jo insert karne ho.
+
+let a7 = [10,15,20,30,45];
+a7.splice(1,3);
+console.log(a7);
+a7.splice(1,3,11,12,13);
